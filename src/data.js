dd0a79ed2f14f825d3af697105d6134b5cab8870
GrupoFemina <adm.vdf6@gmail.com>
2026-01-14 15:32:48 -0300

    Refactor data.js to update card entries

--- src/data.js
+++ src/data.js
@@ -7,99 +7,15 @@ const data = {
     "683615767be3d4dd7675250d": "ATENDIMENTO - LOJA"
   },
   cards: [
-    {
-      name: "CADASTRO NOVO",
-      desc: "<strong>ATIVAÇÃO:</strong> Via pedido.\n<strong>PAGAMENTO:</strong> Boleto 3x.\n<strong>ENTREGA:</strong> endereço do RE.",
-      idList: "683615767be3d4dd7675250e"
-    },
-    {
-      name: "TRANSFERÊNCIA DE CADASTRO - ACOLHIMENTO",
-      desc: "<strong>ATIVAÇÃO:</strong> Pedido em Anexo\n<strong>FORMA DE PAGAMENTO:</strong> Boleto 3x\n<strong>ENTREGA:</strong> No endereço do RE\n<strong>CUPOM:</strong> Inserir cupom vigente",
-      idList: "683615767be3d4dd7675250e"
-    },
-    {
-      name: "REATIVAÇÃO - CESSADO",
-      desc: "<strong>ATIVAÇÃO:</strong> Pedido em Anexo\n<strong>FORMA DE PAGAMENTO:</strong> Boleto 3x \n<strong>ENTREGA:</strong> No endereço do RE\n<strong>CUPOM:</strong> Inserir cupom vigente",
-      idList: "683615767be3d4dd7675250e"
-    },
-    {
-      name: "LIBERAR CADASTRO - AUTOATENDIMENTO",
-      desc: "<strong>Liberação de cadastro</strong> para o RE realizar compras em loja ou no APP.<br><br><strong>STATUS:</strong> [NOVO - CESSADO - TRANSFERÊNCIA]<br><strong>PAGAMENTO:</strong> Boleto 3x.",
-      idList: "683615767be3d4dd7675250e"
-    },
-    {
-      name: "AUTO REATIVAÇÃO",
-      desc: "<strong>RE realizou pedido pelo APP ou em Loja</strong>\n\n<strong>Nº PEDIDO:</strong> XXX.XXX.XXX\n<strong>Ranking:</strong> Adicionar ao ciclo XX",
-      idList: "683615767be3d4dd7675250e"
-    },
-    {
-      name: "PEDIDO COM RETIRADA AGENDADA",
-      desc: "<strong>Nº PEDIDO:</strong> XXX.XXX.XXX\n<strong>CÓD. REV.:</strong> CÓD + NOME RE\n<strong>DATA RETIRAR:</strong> DATA DO PEDIDO + 2 DIAS ÚTEIS",
-      idList: "estoque_coluna_001"
-    },
-    {
-      name: "OCORRÊNCIA DE PRODUTO",
-      desc: "<strong>CÓD. PRODUTO1:</strong> CÓD + NOME DO PRODUTO\n<strong>CÓD. PRODUTO2:</strong>\n<strong>CÓD. PRODUTO3:</strong>",
-      idList: "estoque_coluna_001"
-    },
-    {
-      name: "CANCELAMENTO DE PEDIDO",
-      desc: "<strong>CÓD/NOME:</strong>\n<strong>N° DO PEDIDO:</strong>\n<strong>MOTIVO:</strong>\n\n@‌marcar responsavel estoque @‌marcar gerente estoque",
-      idList: "estoque_coluna_001"
-    },
-    {
-      name: "CARTA DE CORREÇÃO - ENDEREÇO",
-      desc: "Solicito Carta de Correção de Endereço para o pedido, conforme os dados:\n\n<strong>CÓD/NOME:</strong>\n<strong>N° DO PEDIDO:</strong>\n<strong>CEP:</strong>\n<strong>ENDEREÇO:</strong>\n<strong>BAIRRO:</strong>\n<strong>CIDADE:</strong>\n\n<strong>MOTIVO:</strong> (Descrever o motivo com clareza)\n<em>Exemplo: O RE informou um endereço incorreto e solicitou a alteração para o endereço do trabalho.</em>",
-      idList: "estoque_coluna_001"
-     },
-     {
-      name: "CANCELAMENTO PRAZO EXCEDIDO 48 HRS",
-      desc: "Pedido cancelado por tempo limite excedido.",
-      idList: "estoque_coluna_001"
-    },
-    {
-      name: "TRANSFERÊNCIA DE VD - DOCUMENTAÇÃO NECESSÁRIA",
-      desc: "Foto do documento (RG/CNH) frente e verso,\n\nSelfie com o documento junto.\n\nComprovante de endereço atualizado e em seu nome, (caso não tenha, enviar foto do documento do titular junto com o comprovante).\n\nCarta a próprio punho informando que está residindo no endereço atual.\n\n<strong>ENVIAR A DOCUMENTAÇÃO PARA O E-MAIL:</strong><br><strong>\nSe você for de Joinville:</strong> admjoinville@grupofemina.com.br<br><strong>\nSe for de Florianópolis:</strong> admflorianopolis@grupofemina.com.br<br><strong>\nOu se for de Rio do Sul:</strong> admriodosul@grupofemina.com.br",
-      idList: "683616ef6cc0c8d06e9b1dcc"
-    },
-    {
-      name: "TRANSFERIDO",
-      desc: "Olá,\n\nInformamos que seu endereço foi atualizado com sucesso. Agora, o próximo passo é entrar em contato com a VD+ (Venda Direta) da sua localidade para realizar o desbloqueio e a atualização completa do seu cadastro como Revendedor(a) O Boticário.\n\nEnquanto isso, seu cadastro ficará bloqueado temporariamente até que essa etapa seja concluída.\n\nAgradecemos pela parceria até aqui e boas vendas. 💜\n\n<strong>Atenciosamente,</strong>",
-      idList: "683616ef6cc0c8d06e9b1dcc"
-    },
-    {
-      name: "CARTA DE AUTODECLARAÇÃO DE ENDEREÇO",
-      desc: "<em>\"Eu, [Seu Nome Completo], portador do RG nº [Seu RG] e CPF nº [Seu CPF], declaro, que resido atualmente no endereço residencial:</em><br><br>Rua: [Nome da Rua]<br>Número: [Número da Casa/Apartamento]<br>Bairro: [Nome do Bairro]<br>Cidade: [Nome da Cidade]<br>CEP: [CEP]<br><br>Declaro, prestadas são verdadeiras.<br><br>Assinatura: _______________________________",
-      idList: "683616ef6cc0c8d06e9b1dcc"
-    },
-    {
-      name: "PEDIDO À VISTA",
-      desc: "<strong>CP:</strong> [ 20573 - 24125 - 23341 ]<br><strong>FORMA DE PAGAMENTO:</strong> PIX ou CARTÃO DE CRÉDITO 3X<br><strong>IR:</strong> [ CÓD + NOME ]<br><strong>PEDIDO:</strong> XXX.XXX.XXX<br><strong>VALOR:</strong> R$<br><strong>ENTREGA OU RETIRADA:</strong>",
-      idList: "683616ef6cc0c8d06e9b1dcc"
-    },
-    {
-      name: "PIX - FLORIANÓPOLIS",
-      desc: "<strong>RAZÃO SOCIAL:</strong><br>ALQUIMIA COMÉRCIO E REPRESENTAÇÕES LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 1605-5<br>\n<strong>CHAVE PIX:</strong><br>39.265.009/0001-60",
-      idList: "683616ef6cc0c8d06e9b1dcc"          
-    },
-     {
-      name: "PIX - JOINVILLE",
-      desc: "<strong>RAZÃO SOCIAL:</strong><br>FLORAL COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 787-0<br>\n<strong>CHAVE PIX:</strong><br>05.135.693/0001-43",
-      idList: "683616ef6cc0c8d06e9b1dcc"       
-    },
+    // ... (mantenha os cards de estoque e cadastros que você já tem)
     {
       name: "PIX - RIO DO SUL",
       desc: "<strong>RAZÃO SOCIAL:</strong><br>ESSENZA COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>CHAVE PIX:</strong><br>60.193.797/0001-12",
       idList: "683616ef6cc0c8d06e9b1dcc"  
-     }, 
+    },
     {
       name: "CIDADES FLORIPA",
       desc: "<strong>MUNICÍPIOS:</strong><br>ANGELINA<br>ANITÁPOLIS<br>ANTÔNIO CARLOS<br>BIGUAÇU<br>FLORIANÓPOLIS<br>GOVERNADOR CELSO RAMOS<br>PALHOÇA<br>RANCHO QUEIMADO<br>SANTO AMARO DA IMPERATRIZ<br>SÃO JOSÉ<br>SÃO PEDRO DE ALCÂNTARA<br>SÃO BONIFÁCIO",
-      idList: "683616ef6cc0c8d06e9b1dcc"  
-    },
-    {
-      name: "CIDADES JOINVILLE",
-      desc: "<strong>MUNICÍPIOS:</strong><br>JOINVILLE<br>BARRA DO SUL<br>ITAPOÁ<br>GARUVA",
       idList: "683616ef6cc0c8d06e9b1dcc"
     },
     {
@@ -108,50 +24,11 @@ const data = {
       idList: "683616ef6cc0c8d06e9b1dcc"
     },
     {
-      name: "ATENDIMENTO - RE ATIVO",
-      desc: "<strong>CÓD RE/NOME:</strong>\n<strong>CONTATO:</strong>\n<strong>DESCRIÇÃO DO ATENDIMENTO:</strong> Descrever a necessidade do RE.\n<strong>STATUS:</strong>\n<strong>PROMOTORA:</strong> @atendimento",
-      idList: "683615767be3d4dd7675250f"
+      name: "CIDADES JOINVILLE",
+      desc: "<strong>MUNICÍPIOS:</strong><br>JOINVILLE<br>BARRA DO SUL<br>ITAPOÁ<br>GARUVA",
+      idList: "683616ef6cc0c8d06e9b1dcc"
     },
-    {
-      name: "CADASTRO REVENDEDOR - IR (INTENÇÃO DE REVENDA)",
-      desc: "<strong>NOME COMPLETO:</strong>\n<strong>CPF:</strong>\n<strong>BAIRRO:</strong>\n<strong>TELEFONE:</strong>",
-      idList: "683615767be3d4dd7675250f"
-    },
-    {
-      name: "ATENDIMENTO - REATIVAÇÃO",
-      desc: "<strong>CÓD RE + NOME:</strong>\n<strong>CPF:</strong>\n<strong>FONE:</strong>\n<strong>DESCRIÇÃO DO ATENDIMENTO:</strong> Descrever a necessidade do RE.",
-      idList: "683615767be3d4dd7675250f"
-    },
-    {
-      name: "CAR - ATENDIMENTO",
-      desc: "<strong>Central de Atendimento ao Revendedor</strong><br>Atendimento de segunda a sexta, das 8h às 20h.<br>Sábado, das 9h às 16h.<br><br><strong>Whatsapp:</strong> (41) 8735-7095<br><strong>Telefone:</strong> 0800-729-9050<br><em>Ligação gratuita de qualquer lugar do país tanto de telefone fixo quanto de celular.</em>",
-      idList: "683615767be3d4dd7675250f"
-    },
-    {
-      name: "IDENTIFICAÇÃO PARA COMPRAR",
-      desc: "Informamos que, para a realização de compras no Espaço do Revendedor, é obrigatória a apresentação de um documento oficial com foto no caixa.\n\nEssa medida visa garantir a segurança, a identificação correta do revendedor e a integridade do processo de atendimento.\n\nAgradecemos a compreensão e colaboração 💜",
-      idList: "683615767be3d4dd7675250d"
-    },
-    {
-      name: "ARMÁRIO - GUARDA VOLUME",
-      desc: "Para segurança e organização, não é permitido entrar na loja com bolsas ou mochilas.\n\nDisponibilizamos guarda-volumes com chave para guardar seus pertences com segurança.\n\nSempre que possível, venha com o mínimo de itens pessoais.\n\nAgradecemos a compreensão. 💜",
-      idList: "683615767be3d4dd7675250d"
-    },
-        {
-      name: "ACESSO RESTRITO",
-      desc: "Para garantir a segurança, organização e qualidade no atendimento, informamos que o acesso ao Espaço do Revendedor (Loja) é exclusivo para revendedores cadastrados.\n\nNão é permitido o ingresso de acompanhantes ou terceiros durante as compras.\n\nContamos com a compreensão e colaboração de todos para mantermos um ambiente adequado e eficiente para nossos parceiros de negócios.\n\nGratidão 💜",
-      idList: "683615767be3d4dd7675250d"
-    },
-    {
-      name: "ENDEREÇO E HORÁRIO - VD's",
-      desc: "<strong>ESPAÇO DO REVENDEDOR - RIO DO SUL/SC</strong><br>Rodovia BR-470 - do km 140,002 ao km 141,000 - lado par Nº 5350<br>CEP: 89162-875 - Bairro: Valada Itoupava<br><strong>SEGUNDA A SEXTA:</strong> 07:42 às 17:00h<br>📞 (47) 3525-3932 / (47) 3521-6852<br><br><br><strong>ESPAÇO DO REVENDEDOR - ESTREITO/SC</strong><br>Rua Santiago Dantas, Nº 100 - Bairro: Estreito<br>Florianópolis / SC<br><strong>SEGUNDA A SEXTA:</strong> 09:00 às 19:00h<br><strong>SÁBADO:</strong> 08:30 às 11:30h<br>📞 (48) 3039-0976<br>📸 @boticario.vd.florianópolis<br><br><br><strong>ESPAÇO DO REVENDEDOR - PALHOÇA/SC</strong><br>Prefeito Reinoldo Alves, 41 - Shopping Camelão<br><strong>SEGUNDA A SEXTA:</strong> 09:00 às 19:00h<br><strong>SÁBADO:</strong> 08:30 às 11:30h<br>📞 (48) 3058-5573<br>📸 @boticario.vd.florianópolis<br><br><br><strong>ESPAÇO DO REVENDEDOR - JOAÇABA/SC</strong><br>Nº 215 - Bairro: Saguaçu - Joinville/SC<br><strong>SEGUNDA A SEXTA:</strong> 09:00 às 19:00h<br><strong>SÁBADOS:</strong> 09:00 às 12:00h<br>📞 (47) 3439-4219<br>📸 @boticario.joinville",
-      idList: "683615767be3d4dd7675250d"
-    },
-    {
-      name: "CÓDIGOS ESTOQUES",
-      desc: "<table style='border-collapse: collapse; width: 100%; font-family: Arial, sans-serif;'><thead><tr style='background-color: #f2f2f2;'><th style='border: 1px solid #ddd; padding: 8px; text-align: left;'>CÓDIGO - EMPRESA</th><th style='border: 1px solid #ddd; padding: 8px; text-align: left;'>LOCAL</th></tr></thead><tbody><tr><td style='border: 1px solid #ddd; padding: 8px;'>23431 - FLORAL COMERCIO DE COSMETICOS LTDA</td><td style='border: 1px solid #ddd; padding: 8px;'>SAGUAÇU</td></tr><tr><td style='border: 1px solid #ddd; padding: 8px;'>20753 - ALQUIMIA COMERCIO E REPRESENTAÇÕES</td><td style='border: 1px solid #ddd; padding: 8px;'>SANTIAGO DANTAS</td></tr><tr><td style='border: 1px solid #ddd; padding: 8px;'>23432 - FLORAL COMERCIO DE COSMETICOS LTDA</td><td style='border: 1px solid #ddd; padding: 8px;'>PADRE KOLB</td></tr><tr><td style='border: 1px solid #ddd; padding: 8px;'>24104 - ALQUIMIA COMERCIO DE REPRESENTACOES LTDA</td><td style='border: 1px solid #ddd; padding: 8px;'>LOJA DA PALHOÇA</td></tr><tr><td style='border: 1px solid #ddd; padding: 8px;'>22806 - NATURALIS COMERCIO DE COSMETICOS LTDA</td><td style='border: 1px solid #ddd; padding: 8px;'>CD DA PALHOÇA</td></tr><tr><td style='border: 1px solid #ddd; padding: 8px;'>24125 - ESSENZA COMERCIO DE COSMETICOS LTDA</td><td style='border: 1px solid #ddd; padding: 8px;'>RIO DO SUL</td></tr></tbody></table>",
-      idList: "683615767be3d4dd7675250d"
-    }
+    // ... (mantenha o restante dos cards de força de vendas e loja)
   ]
 };
 export default data;
