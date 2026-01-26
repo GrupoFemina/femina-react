54ebadb6f35a22fa6fd645adfe4aa1e04b37a49b
GrupoFemina <adm.vdf6@gmail.com>
2026-01-26 11:24:38 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -88,8 +88,8 @@ const data = {
       idList: "683616ef6cc0c8d06e9b1dcc"
     },
     {
-      name: "PIX RIO DO SUL",
-      desc: "<strong>RAZÃO SOCIAL:</strong><br>ESSENZA COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>CHAVE PIX:</strong><br>60.193.797/0001-12",
+      name: "PIX - FLORIANÓPOLIS",
+      desc: "<strong>RAZÃO SOCIAL:</strong><br>ALQUIMIA COMÉRCIO E REPRESENTAÇÕES LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 1605-5<br>\n<strong>CHAVE PIX:</strong><br>39.265.009/0001-60",
       idList: "683616ef6cc0c8d06e9b1dcc"
     },
      {
@@ -97,7 +97,7 @@ const data = {
       desc: "<strong>RAZÃO SOCIAL:</strong><br>FLORAL COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 787-0<br>\n<strong>CHAVE PIX:</strong><br>05.135.693/0001-43",
       idList: "683616ef6cc0c8d06e9b1dcc"       
     },
-       {
+    {
       name: "PIX RIO DO SUL",
       desc: "<strong>RAZÃO SOCIAL:</strong><br>ESSENZA COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>CHAVE PIX:</strong><br>60.193.797/0001-12",
       idList: "683616ef6cc0c8d06e9b1dcc"  
