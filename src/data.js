ab4f5382fc4a2b2ca1816091ca65d711afa008a5
GrupoFemina <adm.vdf6@gmail.com>
2026-03-27 14:18:34 -0300

    Update PIX descriptions for locations

--- src/data.js
+++ src/data.js
@@ -89,17 +89,17 @@ const data = {
     },
     {
       name: "PIX - FLORIANÓPOLIS",
-      desc: "<strong>RAZÃO SOCIAL:</strong><br>ALQUIMIA COMÉRCIO E REPRESENTAÇÕES LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 1605-5<br>\n<strong>CHAVE PIX:</strong><br>39.265.009/0001-60",
+      desc: "<strong>RAZÃO SOCIAL:</strong><br>ALQUIMIA COMÉRCIO E REPRESENTAÇÕES LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 1605-5<br>\n<strong>CHAVE PIX FLORIANÓPOLIS:</strong><br>39.265.009/0001-60",
       idList: "683616ef6cc0c8d06e9b1dcc"
     },
      {
       name: "PIX - JOINVILLE",
-      desc: "<strong>RAZÃO SOCIAL:</strong><br>FLORAL COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 787-0<br>\n<strong>CHAVE PIX:</strong><br>05.135.693/0001-43",
+      desc: "<strong>RAZÃO SOCIAL:</strong><br>FLORAL COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>BANCO:</strong> Bradesco (237)<br><strong>AGÊNCIA:</strong> 2657<br><strong>CONTA CORRENTE:</strong> 787-0<br>\n<strong>CHAVE PIX JOINVILLE:</strong><br>05.135.693/0001-43",
       idList: "683616ef6cc0c8d06e9b1dcc"       
     },
     {
       name: "PIX - RIO DO SUL",
-      desc: "<strong>RAZÃO SOCIAL:</strong><br>ESSENZA COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>CHAVE PIX:</strong><br>60.193.797/0001-12",
+      desc: "<strong>RAZÃO SOCIAL:</strong><br>ESSENZA COMÉRCIO DE COSMÉTICOS LTDA<br>\n<strong>CHAVE PIX RIO DO SUL:</strong><br>60.193.797/0001-12",
       idList: "683616ef6cc0c8d06e9b1dcc"  
      },
      {
