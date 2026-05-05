a9570ebc2268522db40ccc6585ad956c18c68d6c
GrupoFemina <adm.vdf6@gmail.com>
2026-05-05 10:58:12 -0300

    Update cancellation order description in data.js

--- src/data.js
+++ src/data.js
@@ -44,7 +44,7 @@ const data = {
     },
     {
       name: "CANCELAMENTO DE PEDIDO",
-      desc: "<strong>CANCELAMENTO DE PEDIDO</strong>\n\n<strong>CÓD/NOME:</strong>\n<strong>N° DO PEDIDO:</strong>\n<strong>MOTIVO:</strong>\n\n@‌marcar responsavel estoque @‌marcar gerente estoque",
+      desc: "<strong>CANCELAMENTO DE PEDIDO</strong>\n\n<strong>CÓD/NOME:</strong>\n<strong>N° DO PEDIDO:</strong>\n<strong>MOTIVO:</strong>\n<strong>APR. GERENTE:@marcar seu gerente</strong>\n\n<strong>RESP.ESTOQUE:@marcar contato do estoque</strong>\n\n@‌marcar responsavel estoque @‌marcar gerente estoque",
       idList: "estoque_coluna_001"
     },
     {
